4718d02475be80be2bd70f3bd237226a0374f3be
unknown <x15g029@PC504N004.chiba-fjb.ac.jp>
2015-11-02 15:16:04 +0900

    課題

---
+++ src/jp/ac/chibafjbx15g029/kd116/Main.java
@@ -0,0 +1,13 @@
+package jp.ac.chibafjbx15g029.kd116;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		int i;
+			for(i=0;i<2;i++)
+				System.out.println("あいうえお あいうえお あいうえお\nかきくけこ かきくけこ かきくけこ\nさしすせそ さしすせそ さしすせそ");
+	}
+
+}
