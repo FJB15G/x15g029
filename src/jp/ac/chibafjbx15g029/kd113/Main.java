d7f65d3b2e8eae6f08d1cd2fa60e027605ee0ad2
unknown <x15g029@PC504N004.chiba-fjb.ac.jp>
2015-11-02 15:06:26 +0900

    課題

---
+++ src/jp/ac/chibafjbx15g029/kd113/Main.java
@@ -0,0 +1,13 @@
+package jp.ac.chibafjbx15g029.kd113;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		int i;
+			for(i=0;i<3;i++)
+				System.out.println("*");
+	}
+
+}
