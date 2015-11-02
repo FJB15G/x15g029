5cb335f363e9bbf4e1089d7214fa8872b31ee363
unknown <x15g029@PC504N004.chiba-fjb.ac.jp>
2015-11-02 15:04:19 +0900

    課題

---
+++ src/jp/ac/chibafjbx15g029/kd112/Main.java
@@ -0,0 +1,13 @@
+package jp.ac.chibafjbx15g029.kd112;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		int i;
+			for(i=0;i<100;i++)
+				System.out.println("船橋情報ビジネス専門学校");
+	}
+
+}
